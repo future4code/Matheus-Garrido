ab275b3523b3995353a48916ff6675366d949867
alvesgarrido <matheus.agarrido@gmail.com>
2021-04-19 15:20:58 -0300

    Exercicio Variaveis JS

---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,84 @@
+// Exercicio escrita de códio
+
+//=========== Ex. 1: =========== 
+
+/* var nome
+var idade */
+
+/* console.log(typeof nome, typeof idade) */
+
+//Resulado console: undefined undefined. As variáveis não foram definidas,
+//portanto não há como saber qual o seu tipo.
+
+/* const nome = prompt("Digite o seu nome:")
+let idade = prompt("Digite a sua idade")
+
+console.log(nome, idade) */
+
+//Console mostrou os valores informados pelo usuario.
+
+/* console.log("Ola " + nome + ", voce tem " + idade + " matehanos") */
+
+
+//=========== Ex. 2: =========== 
+
+/* const pergNome = "Informe o NOME do cliente:"
+const pergCpf = "Informe o CPF do cliente:"
+const pergVend = "Informe o ID do vendedor:"
+const pergProd = "Informe o CODIGO do produto:"
+const PergQntd = "Informe a QUANTIDADE do produto:"
+
+let cliente = prompt(pergNome)
+let cpf = prompt(pergCpf)
+let idVendedor = prompt(pergVend)
+let codProduto = prompt(pergProd)
+let qntProduto = prompt(PergQntd)
+
+console.log("1. " + pergNome)
+console.log("Resposta: " + cliente)
+
+console.log("1. " + pergCpf)
+console.log("Resposta: " + cpf)
+
+console.log("1. " + pergVend)
+console.log("Resposta: " + idVendedor)
+
+console.log("1. " + pergProd)
+console.log("Resposta: " + codProduto)
+
+console.log("1. " + PergQntd)
+console.log("Resposta: " + qntProduto) */
+
+
+//=========== Ex. 3: =========== 
+
+/* const pratosPref = ['arrumadinho','feijoada', 'temaki', 'bacalhau em natas', 'brownnie']
+console.log(pratosPref)
+console.log("Essas são as minhas comidas preferidas: ")
+console.log(pratosPref[0])
+console.log(pratosPref[1])
+console.log(pratosPref[2])
+console.log(pratosPref[3])
+console.log(pratosPref[4])
+//Desafio:
+const comidUser = prompt("Qual a sua comida preferida?")
+pratosPref[1] = comidUser
+console.log("Essas são as minhas comidas preferidas: ")
+console.log(pratosPref[0])
+console.log(pratosPref[1])
+console.log(pratosPref[2])
+console.log(pratosPref[3])
+console.log(pratosPref[4]) */
+
+//=========== Ex. 4: =========== 
+
+const perguntas = ['Seu sapato é preto?', 'Você usa óculos?', 'Você já almoçou?']
+const respostas = [false, false, true]
+
+console.log(perguntas[0] + " " + respostas[0])
+console.log(perguntas[1] + " " + respostas[1])
+console.log(perguntas[2] + " " + respostas[2])
+
+
+
+
