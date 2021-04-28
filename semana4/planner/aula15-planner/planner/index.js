9f8a69bfdac683aa028e263747f9e6133400b602
alvesgarrido <matheus.agarrido@gmail.com>
2021-04-27 22:28:43 -0300

    Resolução dos desafios - DOM

--- semana4/planner/aula15-planner/planner/index.js
+++ semana4/planner/aula15-planner/planner/index.js
@@ -1,45 +1,47 @@
+let identificadorTarefa = 1
+let horaInicial = 6
+let horaFinal = 22
+
+function horarios() {
+    const diasSemana = document.getElementsByClassName("semana")
+    for (let i = 0; i < diasSemana.length; i++) {
+        const dia = diasSemana[i]
+        const identificador = dia.getElementsByTagName("div")[0]
+        for (let n = horaInicial; n <= horaFinal; n++) {
+            identificador.innerHTML += `<div id="${identificador.id}${n}">${n}:00</div>`
+        }  
+    }
+}
+
+
 function listaTarefa() {
     const tarefa = document.getElementById('tarefa')
     const diaSemana = document.getElementById('dias-semana')
-    let divDiaSemana = document.getElementById(`${diaSemana.value}`)
-    divDiaSemana.innerHTML += `<p>- ${tarefa.value}</p>`
+    const horario = document.getElementById("horario")
+ 
+ if (tarefa.value !=="") {
+    let divDiaSemana = document.getElementById(`${diaSemana.value}${horario.value}`)
+    divDiaSemana.innerHTML += `<p id="tarefa${identificadorTarefa}" onclick="sublinhaTarefa('tarefa${identificadorTarefa}')">- ${tarefa.value}</p>`
     tarefa.value = ""
-
-
-     /* switch (diaSemana.value) {
-        case 'domingo':
-            divDiaSemana.innerHTML += `<p>- ${tarefa.value}</p>`
-            tarefa.value = ""
-            break
-
-        case 'segunda':
-            divDiaSemana.innerHTML += `<p>- ${tarefa.value}</p>`
-            tarefa.value = ""
-            break
-        
-        case 'terca':
-            divDiaSemana.innerHTML += `<p>- ${tarefa.value}</p>`
-            tarefa.value = ""
-            break
-
-        case 'quarta':
-            divDiaSemana.innerHTML += `<p>- ${tarefa.value}</p>`
-            tarefa.value = ""
-            break
-
-        case 'quinta':
-            divDiaSemana.innerHTML += `<p>- ${tarefa.value}</p>`
-            tarefa.value = ""
-            break
-
-        case 'sexta':
-            divDiaSemana.innerHTML += `<p>- ${tarefa.value}</p>`
-            tarefa.value = ""
-            break
-        default:
-            divDiaSemana.innerHTML += `<p>- ${tarefa.value}</p>`
-            tarefa.value = ""
-            break
-    } */
-
+    identificadorTarefa++
+ } else {
+     alert('Você não informou nenhuma tarefa. Informe uma atividade!')
  }
+}
+
+function sublinhaTarefa(idtAtividade) {
+    const atvidade = document.getElementById(`${idtAtividade}`)
+    atvidade.innerHTML = atvidade.textContent.strike()
+}
+
+function zerarAtividades() {
+    const diasSemana = document.getElementsByClassName("semana")
+    for (let i = 0; i < diasSemana.length; i++) {
+        const dia = diasSemana[i]
+        const identificador = dia.getElementsByTagName("div")[0]
+        identificador.innerHTML = ""
+    }
+    horarios()
+}
+
+
