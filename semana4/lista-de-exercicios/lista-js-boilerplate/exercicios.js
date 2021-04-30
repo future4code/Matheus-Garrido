ff138e533f9a47c8bc5d410bcacca9a7d67db6ed
alvesgarrido <matheus.agarrido@gmail.com>
2021-04-30 17:28:45 -0300

    Exercício 6 - Array de booleanos

--- semana4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
+++ semana4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
@@ -58,6 +58,14 @@ function retornaQuantidadeElementos (array) {
 
 function retornaExpressoesBooleanas() {
    // implemente sua lógica aqui
+   const booleano1 = true
+   const booleano2 = false
+   const booleano3 = !booleano2 
+   const booleano4 = !booleano3 
+
+   const respostas = [booleano1 && booleano2 && !booleano4, (booleano1 && booleano2) || !booleano3, (booleano2 || booleano3) && (booleano4 || booleano1), !(booleano2 && booleano3) || !(booleano1 && booleano3), !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)]
+
+   return respostas
 }
 
 //Exercício 7
