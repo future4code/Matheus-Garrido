5548b2ef80598a894443a63e44d616340a67a4db
alvesgarrido <matheus.agarrido@gmail.com>
2021-05-20 20:14:29 -0300

    Integraçao de API - Escolha randomica de destino de viagem

---
+++ semana7/aplicacao-api-public/aplicacao-api-public/src/styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+
+
+export const MainCont = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const Result = styled.div`
+    .bandeira {
+        width: 200px;
+    }
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+    margin: 20px;
+
+`
