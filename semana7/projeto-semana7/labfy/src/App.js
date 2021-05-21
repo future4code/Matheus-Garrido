56a22be7372540390ce073cea6afda9880b4344d
alvesgarrido <matheus.agarrido@gmail.com>
2021-05-21 18:01:18 -0300

    Labfy - Implementação funcionalidades

---
+++ semana7/projeto-semana7/labfy/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Biblioteca from './Components/Biblioteca/Biblioteca'
+
+export default class App extends React.Component {
+  
+  render() {
+
+    return (
+      <div>
+        <Biblioteca/>
+      </div>
+    )
+  }
+}
