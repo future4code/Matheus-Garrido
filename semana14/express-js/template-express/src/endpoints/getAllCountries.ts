b567ec73e9ccc2bb3bce93a7caf140c0f45e97c1
alvesgarrido <matheus.agarrido@gmail.com>
2021-07-15 17:20:31 -0300

    Exercicios Express - Aula 43

---
+++ semana14/express-js/template-express/src/endpoints/getAllCountries.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express"
+import { countries } from "../data"
+
+export const getAllCountries = (
+    req: Request, 
+    res: Response
+): void => {
+    const result = countries.map(country => ({
+        id: country.id,
+        name: country.name
+    }))
+      
+    //res.send(countries)
+    res.status(200).send(result)
+}
